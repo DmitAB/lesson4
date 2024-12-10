d5a4152b5b3de3b96209f95aaa31966689eb75d1
Dmitriy <zhilexpert@gmail.com>
2024-12-10 23:13:34 +0400

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,13 @@
+from math import inf
+def fake_divide(first, second):
+    if second == 0:
+        return ('ошибка')
+    else:
+        return (first/second)
+
+
+#result1 = fake_divide(69, 3)
+#print(result1)
+
+#result2 = fake_divide(3, 0)
+#print(result2)
